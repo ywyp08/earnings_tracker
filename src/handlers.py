d292866a98cd8dd1fe4d5048e33c6255b2a61f25
ywyp08 <stepa.spilka@gmail.com>
2025-11-30 20:31:37 +0400

    added conversion to czk

--- src/handlers.py
+++ src/handlers.py
@@ -5,10 +5,12 @@ from .utils import *
 
 def earn(args):
     data = load_data()
+    amount_czk = convert_to_czk(args.amount, args.currency)
     entry = {
             "date": datetime.now().strftime("%Y-%m-%d"),
             "amount": args.amount,
-            "currency": args.currency
+            "currency": args.currency,
+            "amount_czk": amount_czk
     }
     data.append(entry)
     save_data(data)
@@ -22,7 +24,7 @@ def report(args):
     
     for entry in data:
         if entry["date"].startswith(time_period):
-            total += entry["amount"]
+            total += entry["amount_czk"]
 
     print(f"Month: {time_period}")
     print(f"Earnings: {total:.2f} CZK")
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import urllib.request
 
 FILE_PATH = os.path.expanduser("./data/earnings.json")
 
@@ -14,3 +15,12 @@ def load_data():
 def save_data(data):
     with open(FILE_PATH, "w") as file:
         json.dump(data, file, indent=4)
+
+
+def convert_to_czk(amount, currency):
+    url = f'https://api.exchangerate-api.com/v4/latest/{currency.upper()}'
+    response = urllib.request.urlopen(url)
+    data = json.load(response)
+    rate = data['rates']['CZK']
+    amount_czk = amount * rate
+    return amount_czk
